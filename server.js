3f70d4a5e16b5ccaceec9aa84a89451e5e8e93d6
Glitch (glitch-blank-node) <none>
2023-03-05 01:12:56 +0000

    🚅🌄 Checkpoint ./server.js:55434288/614

--- server.js
+++ server.js
@@ -19,19 +19,20 @@ var dbFile = "./.data/sqlite.db";
 var exists = fs.existsSync(dbFile);
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(dbFile);
+db.open;
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(function() {
   //if (!exists) {
     console.log("Table Creation");
-    db.run("CREATE TABLE IF NOT EXISTS User(userID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, alcoholFree BOOLEAN DEFAULT false, dairyFree BOOLEAN DEFAULT false, fishFree BOOLEAN DEFAULT false, glutenFree BOOLEAN DEFAULT false, kosher BOOLEAN DEFAULT false, peanutFree BOOLEAN DEFAULT false, soyFree BOOLEAN DEFAULT false, treeNutFree BOOLEAN DEFAULT false, vegan BOOLEAN DEFAULT false, vegetarian BOOLEAN DEFAULT false)");
-    db.run("CREATE TABLE IF NOT EXISTS SavedRecipe(recipeID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, recipeName TEXT NOT NULL, recipeIngredients JSON NOT NULL);");
-    db.run("CREATE TABLE IF NOT EXISTS User_SavedRecipe(userID INTEGER NOT NULL, recipeID INTEGER NOT NULL, CONSTRAINT FK_User_User_SavedRecipe FOREIGN KEY (userID) REFERENCES User(userID), CONSTRAINT FK_SavedRecipe_User_SavedRecipe FOREIGN KEY (recipeID) REFERENCES Recipe(recipeID));");
+    db.run('CREATE TABLE IF NOT EXISTS User(userID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, alcoholFree BOOLEAN DEFAULT false, dairyFree BOOLEAN DEFAULT false, fishFree BOOLEAN DEFAULT false, glutenFree BOOLEAN DEFAULT false, kosher BOOLEAN DEFAULT false, peanutFree BOOLEAN DEFAULT false, soyFree BOOLEAN DEFAULT false, treeNutFree BOOLEAN DEFAULT false, vegan BOOLEAN DEFAULT false, vegetarian BOOLEAN DEFAULT false)');
+    db.run('CREATE TABLE IF NOT EXISTS SavedRecipe(recipeID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, recipeName TEXT NOT NULL, recipeIngredients JSON NOT NULL);');
+    db.run('CREATE TABLE IF NOT EXISTS User_SavedRecipe(userID INTEGER NOT NULL, recipeID INTEGER NOT NULL, CONSTRAINT FK_User_User_SavedRecipe FOREIGN KEY (userID) REFERENCES User(userID), CONSTRAINT FK_SavedRecipe_User_SavedRecipe FOREIGN KEY (recipeID) REFERENCES Recipe(recipeID));');
     
     db.serialize(function() {
       console.log("Table Insert");
       db.run(
-        "INSERT INTO User (username, password) VALUES ('Admin', 'Admin');"
+        'INSERT INTO User (username, password) VALUES ("Admin", "Admin");'
       );
     });
   //} else {
@@ -58,7 +59,7 @@ app.get("/login", function(request, response) {
 app.post("/login", function(request, response) {
   const username = request.body.username;
   const password = request.body.password;
-  const query = "SELECT * FROM User WHERE User.username = " + username + ";"
+  const query = 'SELECT * FROM User WHERE User.username = ' + username + ';'
   
   db.all(query, function(err, user) {
     if (user == undefined) {
@@ -66,6 +67,9 @@ app.post("/login", function(request, response) {
       console.log(user);
       console.log(err);
       response.sendFile(__dirname + "/src/pages/login.html");
+      
+      console.log(db.run('SELECT * FROM User'));
+      
       
     } else if (user.password == password) {
       console.log("Login Successful!");
@@ -91,7 +95,7 @@ app.post("/register", function(request, response) {
   const username = request.body.username;
   const password1 = request.body.password1;
   const password2 = request.body.password2;
-  const query = "INSERT INTO User (username, password) VALUES (" + username + ", " + password1 + ");"
+  const query = 'INSERT INTO User (username, password) VALUES (' + username + ', ' + password1 + ');'
   
   if (password1 == password2) {
     db.all(query, function(err, user) {
