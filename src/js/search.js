062783fe990eb8120b77b7efdee388598816aa24
Glitch (glitch-blank-node) <none>
2023-03-05 03:42:45 +0000

    🍅🏄 Checkpoint ./src/pages/saved-recipe.html:55434288/380 ./src/js/search.js:55434314/944:55437339/2 ./src/pages/search.html:55434184/11538

--- src/js/search.js
+++ src/js/search.js
@@ -9,25 +9,21 @@
 $('#searchButton').on("click", function () {
 	
   var ingredients= $("#ingredient").val(); 
-  
+  var rating=$(#dropDown).val;
   if(ingredients.length>0){
     
     var noCommaIngredients = ingredients.replace(/,/g, '');
     var noCommaIngredients = noCommaIngredients.replace(/./g, '');
     var splitIngredients = noCommaIngredients.split(' ');
-
-    var queryInsertString="";
+    var queryInsertString="api.edamam.com/api/recipes/v2?type=public&q=";
     
-    for (var i = 0; i < splitIngredients.length; i++) {
-      queryInsertString= splitIngredients[i] ;
+    for (var i = 0; i < splitIngredients.length-1; i++) {
+      queryInsertString= queryInsertString+splitIngredients[i]+"%2C%20" ;
     }
-
-    // Join the array back into a string using the join() method
-    var modifiedString = words.join(' ');
-
-    // Output the modified string to the console
-    console.log(modifiedString);
     
+    queryInsertString=queryInsertString[splitIngredients.length-1]+"&app_id=05dae6b2&app_key=fd4342462e88e2274ddb5850559232f5&imageSize=SMALL&random=true"
+    if()
+        queryInsertString=queryInsertString+"&co2EmissionsClass="+rating; 
   }
 
     
@@ -42,3 +38,5 @@ $('#searchButton').on("click", function () {
     });
   
 });
+
+
