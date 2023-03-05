1b22205c6490b390050784207787f9605c713b9a
Glitch (glitch-blank-node) <none>
2023-03-05 03:19:34 +0000

    🍔🐊 Checkpoint ./src/pages/search.html:55434184/10062:55434288/211 ./src/js/search.js:55434314/7802 ./src/pages/settings.html:55434288/19 ./src/pages/saved-recipe.html:55434288/19 ./src/pages/recipe.html:55434288/67 ./src/pages/list.html:55434288/19 ./src/pages/home.html:55434288/19

--- src/js/search.js
+++ src/js/search.js
@@ -9,82 +9,36 @@ function addIngredient(ingredient) {
 
 $('#searchButton').on("click", function () {
 	
-    $.ajax(
-	{	
-		type: "POST",
-		url: "https://api.edamam.com/api/recipes/v2"+ $("#movie").val(),
-		data:'jason',
-        success: function (res) {
-            if(res["Response"]!="False")
-            {   
-                //stash button event handler, if there was a title found then toggle on to allow user to save 
-                $('#stashButton').attr("disabled", false);
-
-                //validate entry available before for title 
-                $("#title").html(res["Title"]);
-
-                if (res["Director"]!= "N/A")
-                    $("#director").html(res["Director"]);
-                else 
-                    $("#director").html("Unavailable ");
-
-                if (res["Released"]!= "N/A")
-                    $("#release").html(res["Released"]);
-                else 
-                    $("#release").html("Unavailable ");
-
-            }
-            else
-            {
-                //stash button even handler disabled if user entered invalid title 
-                $('#stashButton').attr("disabled", true);
-
-                //clear the div/card data is invalid entry 
-                $("#title").html("");
-                $("#director").html("");
-                $("#release").html("");
-                $("#genre").html("");
-                $("#plot").html("");
-
-                alert("Search Entry Not Found or invalid entry");
-            }
-        },
-        error: function (er) {
-            alert("Error, unable to access api")
-        }
-	})
-});
-
-
-$('#saveRecipe').on("click", function () {
-
-//     //retieve data from the prior ajax call that was added to the html 
-//     var title = $("#title").html();
-//     var director=$("#director").html();
-//     var release=$("#release").html();
-//     var genre=$("#genre").html();
-//     var plot=$("#plot").html();
+  var ingredients= $("#ingredient").val(); 
+  
+  if(ingredients.length>0){
     
-//     //post req. 
-//     $.ajax(
-//         {
-//             url: "/stashMovies",
-//             type: "POST",
-//             dataType: 'json',
-//             contentType: "application/json; charset=utf-8",
-//             data: JSON.stringify({
-//                 Title: title,
-//                 Director: director,
-//                 Released: release,
-//                 Genre: genre,
-//                 Plot: plot
-//             }),
-//             //redirect to searches once entered/modal button was clicked (go to movie stash)
-//             success: function (res) {
-//                 location.href='/searches';
-//             },
-//             error: function (er) {
-//                 alert("Error, unable to add to stash ")
-//             }
-//         })
+    var noCommaIngredients = ingredients.replace(/,/g, '');
+    var splitIngredients = noCommaIngredients.split(' ');
+
+    var queryInsertString="";
+    
+    for (var i = 0; i < words.length; i++) {
+      queryInsertString= splitIngredients[i] ;
+    }
+
+    // Join the array back into a string using the join() method
+    var modifiedString = words.join(' ');
+
+    // Output the modified string to the console
+    console.log(modifiedString);
+    
+  }
+
+    
+  
+    $.ajax({
+      url: "process.php", // URL of the server-side script
+      type: "POST",
+      data: { sentence: sentence }, // Send the sentence to the server-side script
+      success: function(response) {
+        alert("Words inserted into database");
+      }
+    });
+  
 });
